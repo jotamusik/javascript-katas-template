ad19fe32c37ee529dcf6efa5c9c22b71f46c028d
Jorge Aguiar Martin <jorge.aguiarmartin@atos.net>
2019-08-30 11:05:10 +0200

    New Test and added readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Javascript Katas
+
+## Descripcion
+
+Plantilla para practicar testing con javascript usando EC6+ configurado con Babel y Jest
--- test/example.test.js
+++ test/example.test.js
@@ -2,13 +2,18 @@
 import Example from "../src/example";
 
 describe('Example Class Definition', function () {
-
-  let example = new Example();
-
   it('Should have defined a exampleVariable member', function () {
+    let example = new Example();
     expect(example.exampleVariable).toBeDefined();
   });
-  it('Should have a exampleVariable getter', function () {
-    expect(example.getExampleVariable()).toEqual(example.exampleVariable);
+  it('Should have an exampleVariable getter', function () {
+    let example = new Example();
+    expect(example.getExampleVariable()).toBe(example.exampleVariable);
+  });
+  it('Should have an exampleVariable setter', function () {
+    let example = new Example();
+    let newExampleVariable = 'exampleModified';
+    example.setExampleVariable(newExampleVariable);
+    expect(example.exampleVariable).toBe(newExampleVariable);
   });
 });
